70f3a3065d83f35b0cadbf8105ff0f5d13ab035d
Азизов Тимур <timmik94@gmail.com>
2018-08-26 11:55:04 +0300

    Добавьте файлы проекта.

---
+++ src/GitHubClient/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GitHubClient
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/GitHubHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
